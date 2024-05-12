3760182330266c5271d58d4057fdee6dff2eef35
ivanmorikov <ivanmorikov48@gmail.com>
2024-05-12 16:53:14 +0300

    Added test case for chance region

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,20 +8,21 @@ import java.util.concurrent.TimeUnit;
 public class Main {
     private final static String PATH_FIREFOX_WEBDRIVER = "drivers/geckodriver.exe";
     private final static String FIREFOX_WEBDRIVER_NAME = "webdriver.gecko.driver";
-    private static final String PATH_OF_GOOGLE_Driver = "drivers/chromedriver-mac-arm64/chromedriver";
+    private static final String PATH_OF_GOOGLE_DRIVER = "drivers/chromedriver-mac-arm64/chromedriver";
     private final static String GOOGLE_WEBDRIVER_NAME = "webdriver.chrome.driver";
 
     public static void main(String[] args) {
         //System.setProperty(GOOGLE_WEBDRIVER_NAME, PATH_OF_GOOGLE_Driver);
-        System.setProperty(GOOGLE_WEBDRIVER_NAME, PATH_OF_GOOGLE_Driver);
+        System.setProperty(GOOGLE_WEBDRIVER_NAME, PATH_OF_GOOGLE_DRIVER);
         WebDriver driver = new ChromeDriver();
         //WebDriver driver = new FirefoxDriver();
         driver.get("https://megamarket.ru");
-        driver.manage().window().maximize();
+//        driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
-        driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(01, TimeUnit.SECONDS);
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         TestCases testCases = new TestCases(driver);
-        testCases.searchProduct();
+//        testCases.searchProduct();
+        testCases.changeAdress();
     }
 }
--- src/main/java/org/example/TestCases.java
+++ src/main/java/org/example/TestCases.java
@@ -13,8 +13,23 @@ public class TestCases {
     }
 
     public void searchProduct() {
-        WebElement input = driver.findElement(By.xpath("//input[@placeholder='Искать товары']"));//name locator for text box
+        WebElement input = driver.findElement(By.xpath("//input[@placeholder='Искать товары']"));
         input.sendKeys("утюг");
         input.sendKeys(Keys.ENTER);
+    }
+
+    public void changeAdress() {
+//        WebElement input = driver.findElement(By.xpath("//input[@class='svg-icon mobile-header-delivery-address__icon']"));
+
+        WebElement buttonCookie = driver.findElement(By.xpath("//button[@class='cookie__button c-button c-button_theme_primary c-button_size_small']"));
+        buttonCookie.click();
+        WebElement button = driver.findElement(By.xpath("//button[@class='btn-bordered header-region-selector-view__footer-cancel xs']"));
+        button.click();
+        WebElement input = driver.findElement(By.xpath("//input[@placeholder='Регион или город']"));
+        input.sendKeys(" Санкт-Петербург");
+        WebElement submitRegion = driver.findElement(By.xpath("//button[@class='header-region-selector-change__form-submit btn']"));
+        WebElement inputRegion = driver.findElement(By.xpath("//div[@class='select field opened filterable lg']/div[2]/ul/li[1]"));
+        inputRegion.click();
+        submitRegion.click();
     }
 }
